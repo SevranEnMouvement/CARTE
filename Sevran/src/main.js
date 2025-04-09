e45f1a71c4fcc959ef73ba75923c070d4329d84a
Inès'S <ines.sogadzi@hotmail.fr>
2025-04-09 15:51:20 +0200

    :bug: adresse

--- Sevran/src/main.js
+++ Sevran/src/main.js
@@ -2,13 +2,13 @@
 //import * as YUKA from "yuka";
 
 //import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { MapControls } from "three/examples/jsm/controls/MapControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { MapControls } from "./three/examples/jsm/controls/MapControls.js";
+import { GLTFLoader } from "./three/examples/jsm/loaders/GLTFLoader.js";
 //import { modelScale } from "three/tsl";
 import {
   CSS2DRenderer,
   CSS2DObject,
-} from "three/examples/jsm/renderers/CSS2DRenderer.js";
+} from "./three/examples/jsm/renderers/CSS2DRenderer.js";
 //import { update } from "three/examples/jsm/libs/tween.module.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
