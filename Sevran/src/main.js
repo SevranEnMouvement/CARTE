4f02891be9459f45305723aa290d1f0c9c76b62a
Inès'S <ines.sogadzi@hotmail.fr>
2025-06-16 13:49:40 +0200

    🎉

--- Sevran/src/main.js
+++ Sevran/src/main.js
@@ -1,10 +1,15 @@
-import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls as MapControls } from "https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three";
+import * as YUKA from "yuka";
+
+//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { MapControls } from "./js/three/examples/jsm/controls/MapControls.js";
+import { GLTFLoader } from "./js/three/examples/jsm/loaders/GLTFLoader.js";
+//import { modelScale } from "three/tsl";
 import {
   CSS2DRenderer,
   CSS2DObject,
-} from "https://unpkg.com/three@0.158.0/examples/jsm/renderers/CSS2DRenderer.js";
+} from "./js/three/examples/jsm/renderers/CSS2DRenderer.js";
+//import { update } from "three/examples/jsm/libs/tween.module.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -38,6 +43,78 @@ controls.maxTargetRadius = 1200;
 //positionnement camera
 camera.position.set(0, 1500, 4000);
 camera.lookAt(0, 0, 0);
+
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = "absolute";
+labelRenderer.domElement.style.top = "0px";
+labelRenderer.domElement.style.pointerEvents = "none";
+document.body.appendChild(labelRenderer.domElement);
+
+//train
+const vehicleGeometry = new THREE.ConeGeometry(51, 55, 58);
+vehicleGeometry.rotateX(Math.PI * 0.5);
+const vehicleMaterial = new THREE.MeshNormalMaterial();
+const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial);
+vehicleMesh.matrixAutoUpdate = false;
+scene.add(vehicleMesh);
+
+const vehicle = new YUKA.Vehicle();
+
+vehicle.setRenderComponent(vehicleMesh, sync);
+
+function sync(entity, renderComponent) {
+  renderComponent.matrix.copy(entity.worldMatrix);
+}
+
+const path = new YUKA.Path();
+path.add(new YUKA.Vector3(10000, 0, -3450));
+path.add(new YUKA.Vector3(-500, 0, 620));
+path.add(new YUKA.Vector3(-700, 0, 800));
+path.add(new YUKA.Vector3(-2000, 0, 2500));
+console.log(path.current());
+path.add(new YUKA.Vector3(2000, 0, -400));
+path.add(new YUKA.Vector3(500, 0, 0));
+path.add(new YUKA.Vector3(0, 0, 1050));
+path.add(new YUKA.Vector3(-500, 0, 550));
+path.add(new YUKA.Vector3(-1500, 0, 1000));
+path.add(new YUKA.Vector3(-3000, 0, 2050));
+
+path.loop = true;
+
+vehicle.position.copy(path.current());
+vehicle.updateOrientation = true;
+vehicle.rotationType = 1;
+
+vehicle.maxSpeed = 5500;
+
+const followPathBehavior = new YUKA.FollowPathBehavior(path, 0.5);
+vehicle.steering.add(followPathBehavior);
+
+const onPathBehavior = new YUKA.OnPathBehavior(path);
+onPathBehavior.radius = 2;
+vehicle.steering.add(onPathBehavior);
+
+const entityManager = new YUKA.EntityManager();
+entityManager.add(vehicle);
+
+const position = [];
+for (let i = 0; i < path._waypoints.length; i++) {
+  const waypoint = path._waypoints[i];
+  position.push(waypoint.x, waypoint.y, waypoint.z);
+}
+
+const lineGeometry = new THREE.BufferGeometry();
+lineGeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(position, 3)
+);
+
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000000 });
+const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+scene.add(lines);
+
+const time = new YUKA.Time();
 
 const labelRenderer = new CSS2DRenderer();
 labelRenderer.setSize(window.innerWidth, window.innerHeight);
@@ -143,7 +220,7 @@ window.addEventListener("mousedown", function onDocumentMouseDown(event) {
 
 const loader = new GLTFLoader();
 
-loader.load("/sevran.gltf", function (glb) {
+loader.load("./sevran.gltf", function (glb) {
   const model = glb.scene;
   scene.add(model);
 });
